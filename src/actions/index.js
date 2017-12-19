3c4d4433018f5f909201d0a6d41b96f00351d12a
Borisstoy <boris.stoy@gmail.com>
2017-12-19 16:27:04 +0100

    :computer: Set up index, start /new

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_RUL = 'http://reduxblog.herokuapp.com/api'
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=RENNESDU35'
 
 export function fetchPosts() {
