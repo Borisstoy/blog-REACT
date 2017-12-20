0edad99e834c640df412a92cfc16df3d97072768
Borisstoy <boris.stoy@gmail.com>
2017-12-20 16:21:25 +0100

    :computer: Create posts form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render () {
     return (
       <div>
@@ -10,10 +24,29 @@ class PostsNew extends Component {
             Back to posts lists
           </Link>
         </div>
-        PostNew!
+        <h2>Create a post</h2>
+       <form>
+          <Field
+            label="Title"
+            name="title"
+            component={this.renderField}
+          />
+          <Field
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field
+            label="Content"
+            name="content"
+            component={this.renderField}
+          />
+       </form>
       </div>
     )
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
