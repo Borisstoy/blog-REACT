56f667f6c0a36dd4861c4786a4ed1d027ffeea43
Borisstoy <boris.stoy@gmail.com>
2017-12-21 12:03:32 +0100

    :computer: Mount specific post inside post#show component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -4,14 +4,23 @@ import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    // provided by React router
+    const { id } = this.props.match.params.id;
+    this.props.fetchPost(id);
+  }
+
   render () {
     return (
       <div>
         Posts Show!
       </div>
     );
-  };
+  }
 }
 
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
 
-export default PostsShow;
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
