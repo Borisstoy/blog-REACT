fb493770785f0608d3efd209a2a998f6e41bed82
Borisstoy <boris.stoy@gmail.com>
2017-12-24 15:12:06 +0100

    :computer: Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry:
+    path.join(__dirname, 'src', 'index.js')
+  ,
   output: {
     path: __dirname,
     publicPath: '/',
@@ -10,17 +13,31 @@ module.exports = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel-loader',
+      loader: 'babel',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
     }]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
-};
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      mangle: true,
+      sourcemap: false,
+      beautify: false,
+      dead_code: true
+    })
+  ]
+}
