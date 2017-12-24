2b4fefc4851c53ed9295a32f4484dcb1745d3749
Borisstoy <boris.stoy@gmail.com>
2017-12-24 15:16:25 +0100

    :computer: Tweak webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,20 @@
-const path = require('path');
-const webpack = require('webpack');
-
-module.exports = {
-  entry:
-    path.join(__dirname, 'src', 'index.js')
-  ,
+var path = require('path');
+var config = {
+  entry: path.resolve(__dirname, '../app/main.js'),
   output: {
-    path: __dirname,
-    publicPath: '/',
+    path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
+      test: /\.js$/,
+
+      // There is not need to run the loader through
+      // vendors
+      exclude: [node_modules_dir],
+      loader: 'babel'
     }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      mangle: true,
-      sourcemap: false,
-      beautify: false,
-      dead_code: true
-    })
-  ]
-}
+  }
+};
+
+module.exports = config;
